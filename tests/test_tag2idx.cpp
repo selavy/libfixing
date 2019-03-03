6569940814f0c8fe7fef1c10df0cf42da553f155
Peter Lesslie <pclesslie@gmail.com>
2019-03-03 12:26:17 -0800

    Begin moving code into library

--- tests/test_tag2idx.cpp
+++ tests/test_tag2idx.cpp
@@ -3,15 +3,7 @@
 
 #include <vector>
 #include <algorithm>
-#include <boost/mpl/vector.hpp>
-#include <boost/mpl/vector_c.hpp>
-#include <boost/mpl/unique.hpp>
-#include <boost/mpl/placeholders.hpp>
-#include <boost/mpl/equal.hpp>
-#include <boost/mpl/sort.hpp>
-#include <boost/mpl/int.hpp>
-#include <boost/mpl/begin_end.hpp>
-#include <boost/mpl/next_prior.hpp>
+#include "fixing.h"
 
 namespace v2 {
 
@@ -38,15 +30,6 @@ using CustomTag        = FixTag<7999>;
 } /*FIX*/
 
 } /*v2*/
-
-template <class... Tags>
-using MakeTags = typename boost::mpl::unique<
-    typename boost::mpl::sort<boost::mpl::vector<Tags...>>::type,
-    boost::is_same<
-        boost::mpl::placeholders::_1,
-        boost::mpl::placeholders::_2
-    >
->::type;
 
 using Tags = MakeTags<
     v2::FIX::MsgType,
@@ -79,49 +62,6 @@ using ExpectedTags = boost::mpl::vector<
     v2::FIX::SecurityExchange,
     v2::FIX::CustomTag
     >;
-
-template <class First, class Begin, class End>
-struct FindTag {
-    static int call(int tag) {
-        using Length = typename boost::mpl::distance<Begin, End>::type;
-        using Middle = typename boost::mpl::advance<Begin, boost::mpl::int_<Length::value / 2>>::type;
-        using MiddleNext = typename boost::mpl::next<Middle>::type;
-        using MiddleTag = typename Middle::type;
-        // using BeginTag = typename Begin::type;
-        // using EndPrior = typename boost::mpl::prior<End>::type;
-        // using EndTag = typename EndPrior::type;
-
-        // printf("\tChecking tag=%d in range [%d, %d] against %d, sequence length = %ld\n",
-        //         tag, BeginTag::value, EndTag::value, MiddleTag::value, Length::value);
-        if (tag < MiddleTag::value) {
-            return FindTag<First, Begin, Middle>::call(tag);
-        } else if (tag == MiddleTag::value) {
-            using Index = typename boost::mpl::distance<First, Middle>::type;
-            return static_cast<int>(Index::value);
-        } else {
-            return FindTag<First, MiddleNext, End>::call(tag);
-        }
-    }
-};
-
-template <class First, class End>
-struct FindTag<First, End, End> {
-    static int call(int tag) {
-        return -1;
-    }
-};
-
-template <class Seq>
-int tag2idx(int tag) {
-    using Begin = typename boost::mpl::begin<Seq>::type;
-    using End   = typename boost::mpl::end<Seq>::type;
-    return FindTag<Begin, Begin, End>::call(tag);
-}
-
-template <class Seq>
-bool find_tag(int tag) {
-    return tag2idx<Seq>(tag) >= 0;
-}
 
 TEST_CASE("FindTag", "[fix]")
 {
