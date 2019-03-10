817c91ea5909581de50b8001e4895011ff0fe3fa
Peter Lesslie <pclesslie@gmail.com>
2019-03-10 10:07:07 -0700

    Support 5 digit tags

--- include/internal/fix_parser.h
+++ include/internal/fix_parser.h
@@ -151,20 +151,23 @@ private:
         const int r2 = (c[0] - '0')*10 + (c[1] - '0')*1;
         const int r3 = (c[0] - '0')*100 + (c[1] - '0')*10 + (c[2] - '0')*1;
         const int r4 = (c[0] - '0')*1000 + (c[1] - '0')*100 + (c[2] - '0')*10 + (c[3] - '0')*1;
+        const int r5 = (c[0] - '0')*10000 + (c[1] - '0')*1000 + (c[2] - '0')*100 + (c[3] - '0')*10 + (c[4] - '0')*1;
 
         const int e1 = c[1] == '=';
         const int e2 = c[2] == '=';
         const int e3 = c[3] == '=';
         const int e4 = c[4] == '=';
+        const int e5 = c[5] == '=';
 
         const bool c1 =  e1;
         const bool c2 = !e1 &  e2;
         const bool c3 = !e1 & !e2 & e3;
         const bool c4 = !e1 & !e2 & !e3 & e4;
+        const bool c5 = !e1 & !e2 & !e3 & !e4 & e5;
 
-        assert((c1 || c2 || c3 || c4) && "couldn't find '='");
-        const int tag = c1*r1 | c2*r2 | c3*r3 | c4*r4;
-        const int adv = c1*2  | c2*3  | c3*4  | c4*5 ;
+        assert((c1 || c2 || c3 || c4 || c5) && "couldn't find '='");
+        const int tag = c1*r1 | c2*r2 | c3*r3 | c4*r4 | c5*r5;
+        const int adv = c1* 2 | c2* 3 | c3* 4 | c4* 5 | c5* 6;
         return { tag, adv };
     }
 
@@ -189,6 +192,10 @@ private:
         } else if (c[4] == '=') {
             const int tag = (c[0] - '0')*1000 + (c[1] - '0')*100 + (c[2] - '0')*10 + (c[3] - '0')*1;
             const int adv = 5;
+            return { tag, adv };
+        } else if (c[5] == '=') {
+            const int tag = (c[0] - '0')*10000 + (c[1] - '0')*1000 + (c[2] - '0')*100 + (c[3] - '0')*10 + (c[4] - '0')*1;
+            const int adv = 6;
             return { tag, adv };
         } else {
             FIXING_UNREACHABLE();
--- tests/test_fix_parser.cpp
+++ tests/test_fix_parser.cpp
@@ -11,6 +11,7 @@ namespace MyFIX {
 
 using MissingTag = FixTag<75>;
 using CustomTag  = FixTag<7999>;
+using CustomTag2 = FixTag<80001>;
 
 } /*MyFIX*/
 
@@ -25,12 +26,14 @@ using Tags = fixing::MakeTags<
             FIX::TransactTime,
             MyFIX::MissingTag,
             FIX::ExecType,
-            MyFIX::CustomTag
+            MyFIX::CustomTag,
+            MyFIX::CustomTag2
             >;
 
 TEST_CASE("Parse FIX message #1", "[fix_parser]")
 {
-    std::string msg = "8=FIX.4.2|9=253|35=8|128=XYZ|34=124|49=CCG|56=ABC_DEFG04|52=20100208-18:51:42|55=RRC|37=NF 0015/02082010|11=NF 0015/02082010|17=0|20=0|39=0|150=0|54=2|38=1000|40=2|44=55.3600|59=0|31=0|32=0|14=0|6=0|151=1000|60=20100208-18:51:42|58=New order|30=N|1=ABC123ZYX|207=N|7999=Peter's Custom Tag That Has Data|47=A|10=037|";
+    // TODO: update length and checksum to be conforming FIX
+    std::string msg = "8=FIX.4.2|9=253|35=8|128=XYZ|34=124|49=CCG|56=ABC_DEFG04|52=20100208-18:51:42|55=RRC|37=NF 0015/02082010|11=NF 0015/02082010|17=0|20=0|39=0|150=0|54=2|38=1000|40=2|44=55.3600|59=0|31=0|32=0|14=0|6=0|151=1000|60=20100208-18:51:42|58=New order|30=N|1=ABC123ZYX|207=N|7999=Peter's Custom Tag That Has Data|47=A|80001=This is a long tag|10=037|";
     std::replace(msg.begin(), msg.end(), '|', '\001');
 
     Parser<Tags> p;
@@ -62,6 +65,7 @@ TEST_CASE("Parse FIX message #1", "[fix_parser]")
     // auto targetcompid = p.get<FIX::TargetCompID>();
     // REQUIRE(targetcompid.empty());
 
-    auto customtag = p.get<MyFIX::CustomTag>();
-    REQUIRE(customtag == "Peter's Custom Tag That Has Data");
+    REQUIRE(p.get<MyFIX::CustomTag>() == "Peter's Custom Tag That Has Data");
+
+    REQUIRE(p.get<MyFIX::CustomTag2>() == "This is a long tag");
 }
